0a5c358fda3f659be0c778e1a3ed223e3b512bc4
ahmetaslan13 <ahmetaslan.work@gmail.com>
2023-08-21 12:52:57 -0400

    feature

--- app/stacks/rg/variables.tf
+++ app/stacks/rg/variables.tf
@@ -1,18 +1,15 @@
 // *** Resource Group Variables ***
 
-// Name of the Azure Resource Group.
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group."
   type        = string
 }
 
-// Location where the Azure Resource Group will be created.
 variable "location" {
   description = "Location where the Azure Resource Group will be created."
   type        = string
 }
 
-// Tags to associate with the Azure Resource Group.
 variable "tags" {
   description = "Tags to associate with the Azure Resource Group."
   type        = map(any)
